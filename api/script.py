64abaff50f5479d1febbeea704a2f156d575994e
Damian Raczkowski <57325417+DocentSzachista@users.noreply.github.com>
2022-06-30 10:43:29 +0200

    comment out to check if anything of it works in github action

--- api/script.py
+++ api/script.py
@@ -9,21 +9,21 @@ import json
 def update_repository():
 
   
-    config = get_json_data("userPreferences.json")
+#     config = get_json_data("userPreferences.json")
     
-    tournaments = dict()
-    for parameter in config:
-        print(parameter)
-        retrieved_data = data_retrieval_wrapper(  
-            tournament_city = parameter["tournament_city"],
-            country_state = parameter["country_state"],
-            tournament_status = parameter["tournament_status"],
-            tempo_option = parameter["tournament_tempo"],
-            tournament_name = parameter["tournament_name"],
-            divided= True
-        )
-        tournaments[parameter["country_state"]] = retrieved_data
-    dump_score_to_file(tournaments, "../docs/htmlLists/tournaments.json")
+#     tournaments = dict()
+#     for parameter in config:
+#         print(parameter)
+#         retrieved_data = data_retrieval_wrapper(  
+#             tournament_city = parameter["tournament_city"],
+#             country_state = parameter["country_state"],
+#             tournament_status = parameter["tournament_status"],
+#             tempo_option = parameter["tournament_tempo"],
+#             tournament_name = parameter["tournament_name"],
+#             divided= True
+#         )
+#         tournaments[parameter["country_state"]] = retrieved_data
+#     dump_score_to_file(tournaments, "../docs/htmlLists/tournaments.json")
     # parse_html_to_file(parameter["country_state"], retrieved_data, style_link='<link rel="stylesheet" href="../table_styles.css">')
     # subprocess.run(["git", "add", "../docs"])
     # subprocess.run(["git", "commit", "-m" f'"Tournaments update: {datetime.today()}"'])
