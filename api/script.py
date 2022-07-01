3213778dc94f72d155eaa3559e497971dd229e1a
Damian Raczkowski <57325417+DocentSzachista@users.noreply.github.com>
2022-07-01 13:12:31 +0200

    Update directory for userPreferences.json
    I want to reflect place where the file is being stored by point of script working directory

--- api/script.py
+++ api/script.py
@@ -6,31 +6,29 @@ import os
 import subprocess
 import json
 
-#def update_repository():
+def update_repository():
 
   
-#     config = get_json_data("userPreferences.json")
-    
-#     tournaments = dict()
-#     for parameter in config:
-#         print(parameter)
-#         retrieved_data = data_retrieval_wrapper(  
-#             tournament_city = parameter["tournament_city"],
-#             country_state = parameter["country_state"],
-#             tournament_status = parameter["tournament_status"],
-#             tempo_option = parameter["tournament_tempo"],
-#             tournament_name = parameter["tournament_name"],
-#             divided= True
-#         )
-#         tournaments[parameter["country_state"]] = retrieved_data
-#     dump_score_to_file(tournaments, "../docs/htmlLists/tournaments.json")
-    # parse_html_to_file(parameter["country_state"], retrieved_data, style_link='<link rel="stylesheet" href="../table_styles.css">')
-    # subprocess.run(["git", "add", "../docs"])
-    # subprocess.run(["git", "commit", "-m" f'"Tournaments update: {datetime.today()}"'])
-    # subprocess.run(["git", "push"])
+    config = get_json_data("./api/userPreferences.json")
+    tournaments = dict()
+    for parameter in config:
+        print(parameter)
+        retrieved_data = data_retrieval_wrapper(  
+            tournament_city = parameter["tournament_city"],
+            country_state = parameter["country_state"],
+            tournament_status = parameter["tournament_status"],
+            tempo_option = parameter["tournament_tempo"],
+            tournament_name = parameter["tournament_name"],
+            divided= True
+        )
+        tournaments[parameter["country_state"]] = retrieved_data
+    dump_score_to_file(tournaments, "../docs/htmlLists/tournaments.json")
+    parse_html_to_file(parameter["country_state"], retrieved_data, style_link='<link rel="stylesheet" href="../table_styles.css">')
+    subprocess.run(["git", "add", "../docs"])
+    subprocess.run(["git", "commit", "-m" f'"Tournaments update: {datetime.today()}"'])
+    subprocess.run(["git", "push"])
 
     
 if __name__ == "__main__":
-    print(os.getcwd())
-#     update_repository()
+    update_repository()
 
